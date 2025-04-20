7f147fe85de634aa5373f363bdfc91fc4605937d
AbdumalikovAbbos <abbosabdumalikov960@gmail.com>
2025-04-20 22:10:57 +0500

    dashboard_route

---
+++ app/ui/font.ts
@@ -0,0 +1,22 @@
+// import { Lusitana } from "@/app/ui/fonts";
+// import { Inter, Lusitana } from "next/font/google";
+
+// export const inter = Inter({
+//   subsets: ["latin"],
+// });
+
+// export const lusitana = Lusitana({
+//   subsets: ["latin"],
+// //   weight: ["400", "700"],
+// });
+
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
